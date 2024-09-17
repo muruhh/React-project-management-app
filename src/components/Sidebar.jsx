1b50eb11a869a1c0fe5b46cd54c594e482885357
kareem mohamed <kareem_mohamed94@outlook.com>
2024-09-17 15:47:59 +0300

    Creating Sidebar.jsx component

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,10 @@
+export default function Sidebar() {
+  return (
+    <aside className="h-screen bg-gray-800 w-1/4 p-10 rounded-r-xl rounded-br-none">
+      <h2 className="font-bold text-slate-300 text-3xl">Projects</h2>
+      <button className="font-bold p-3 rounded-lg mt-6 bg-blue-700 text-white cursor-pointer drop-shadow hover:bg-blue-500 transition-all">
+        +Add Project
+      </button>
+    </aside>
+  );
+}
