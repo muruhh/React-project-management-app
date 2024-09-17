a1d3f70886eb36285baa4fa572d45c36e08c6ceb
kareem mohamed <kareem_mohamed94@outlook.com>
2024-09-17 16:16:28 +0300

    Enhancing Button.jsx component

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,6 +1,9 @@
-export default function Button({ classes, children }, ...props) {
+export default function Button({ extrClasses, children }, ...props) {
+  let defaultClasses =
+    "font-bold p-3 rounded-lg mt-6 bg-blue-700 text-white cursor-pointer drop-shadow hover:bg-blue-500 transition-all ";
+
   return (
-    <button className={classes} {...props}>
+    <button className={defaultClasses + extrClasses} {...props}>
       {children}
     </button>
   );
--- src/components/NoProjects.jsx
+++ src/components/NoProjects.jsx
@@ -10,13 +10,7 @@ export default function NoProjects() {
       />
       <h2 className="mt-3">No Projects to be displayed yet</h2>
       <p className="mt-3">Add your first project</p>
-      <Button
-        classes={
-          "font-bold p-3 rounded-lg mt-6 bg-blue-700 text-white cursor-pointer drop-shadow hover:bg-blue-500 transition-all"
-        }
-      >
-        +Add Project
-      </Button>
+      <Button>+Add Project</Button>
     </div>
   );
 }
